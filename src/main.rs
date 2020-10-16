0a6ea11caef4af0bc39eee8b8b8f611035d27223
Curtis Alexander <calex@calex.org>
2020-10-16 17:41:43 -0500

    Swap out enum for bitflags

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
-use std::{ffi::c_void, mem::transmute};
+use std::{ffi::c_void, fmt, mem::transmute};
+
+#[macro_use]
+extern crate bitflags;
 
 // type aliases
 type HModule = *const c_void;
 type FarProc = *const c_void;
 type SetThreadExecutionState = extern "stdcall" fn(esFlags: ExecutionState) -> ExecutionState ;
 
+
+bitflags! {
+    pub struct ExecutionState: u32 {
+    // Away mode should be used only by media-recording and media-distribution applications that must perform critical background processing on desktop computers while the computer appears to be sleeping.
+    // const ES_AWAYMODE_REQUIRED = 0x00000040;
+    // Informs the system that the state being set should remain in effect until the next call that uses ES_CONTINUOUS and one of the other state flags is cleared.
+    const ES_CONTINUOUS = 0x80000000;
+    // Forces the display to be on by resetting the display idle timer.
+    const ES_DISPLAY_REQUIRED = 0x00000002;
+    // Forces the system to be in the working state by resetting the system idle timer.
+    const ES_SYSTEM_REQUIRED = 0x00000001;
+    // This value is not supported. If ES_USER_PRESENT is combined with other esFlags values, the call will fail and none of the specified states will be set.
+    // const ES_USER_PRESENT = 0x00000004;
+    }
+}
+
+impl fmt::Display for ExecutionState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:#?} ({:#X})", &self, &self)
+    }
+}
+/*
 #[derive(Debug, Clone)]
 #[allow(non_camel_case_types)]
 enum ExecutionState {
@@ -19,6 +44,17 @@ enum ExecutionState {
     // This value is not supported. If ES_USER_PRESENT is combined with other esFlags values, the call will fail and none of the specified states will be set.
     // ES_USER_PRESENT = 0x00000004
 }
+*/
+/*
+impl std::ops::BitOr for ExecutionState {
+    type Output = u32 ;
+
+    fn bitor(self, rhs: Self) -> u32 {
+        (self as u32) | (rhs as u32)
+    }
+
+}
+*/
 
 // External functions
 extern "stdcall" {
@@ -47,8 +83,8 @@ impl StayAwake {
 
 impl Drop for StayAwake {
     fn drop(&mut self) {
-        println!("Resetting thread execution state to ES_CONTINUOUS(0x80000000)");
-        let _ = (self.ste)(ExecutionState::ES_CONTINUOUS);
+        let prev_execution_state = (self.ste)(ExecutionState::ES_CONTINUOUS);
+        println!("Resetting thread execution state from {} to ES_CONTINUOUS (0x80000000)", prev_execution_state);
     }
 }
 
@@ -57,12 +93,11 @@ fn main() {
     let sa = StayAwake::new();
     let ste = sa.ste ;
 
-    // set thread execution state
-    println!("Testing out setting ES_CONTINUOUS | ES_SYSTEM_REQUIRED");
-    let prev_execution_state: ExecutionState = ste(ExecutionState::ES_CONTINUOUS);
-    // let prev_execution_state: ExecutionState = ste(ExecutionState::ES_CONTINUOUS | ExecutionState::ES_SYSTEM_REQUIRED);
+    // state to set
+    let next_thread_exec_state = ExecutionState::ES_CONTINUOUS | ExecutionState::ES_DISPLAY_REQUIRED;
 
-    // returned value from a call to SetThreadExecutionState is the prior state
-    println!("The previous ExecutionState was {:?}({:#X})", prev_execution_state.clone(), prev_execution_state as u32);
+    // set thread execution state
+    let prev_thread_exec_state: ExecutionState = ste(next_thread_exec_state);
+    println!("Going from thread execution state {:#?} to {:#?}", prev_thread_exec_state, next_thread_exec_state); 
 
 }
