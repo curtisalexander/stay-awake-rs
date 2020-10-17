814e79a1959f68c0e86011d12f65f1261ca97af6
Curtis Alexander <calex@calex.org>
2020-10-16 23:08:34 -0500

    Fix renaming

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use structopt::StructOpt;
 
-use stay_awake_rs::Args;
+use stay_awake::Args;
 use std::process;
 
 fn main() {
     let args: Args = Args::from_args();
-    if let Err(e) = stay_awake_rs::run(args) {
+    if let Err(e) = stay_awake::run(args) {
         println!("Stopping with error: {}", e);
         process::exit(1);
     }
